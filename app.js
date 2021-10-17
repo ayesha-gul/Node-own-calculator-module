0b689cad643ddb8b15b112f0b3096603cf4b149c
ayesha <fxolbird@gmail.com>
2021-10-17 03:46:10 -0700

    chnaged:

---
+++ app.js
@@ -0,0 +1,8 @@
+let calc = require('./calc.js')
+
+
+sult = calc.add(2,9)
+result = calc.sub(199,99)
+
+console.log('The output is'+ " " +sult);
+console.log('The output is'+ " " +result);
---
+++ calc.js
@@ -0,0 +1,8 @@
+function add(a,b) {
+    return a+b;
+    
+}
+exports.sub = function sub(x,y) {
+    return x-y;
+}
+exports.add = add;
